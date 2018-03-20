b9c54e4508c4ef636e297cdf9eddf44a6bc5d9ed
Michal Radzewicz <radzewicz.micha@gmail.com>
2018-03-20 15:50:59 +0100

    First builder

--- 1_Builder/src/Main.java
+++ 1_Builder/src/Main.java
@@ -1,7 +1,24 @@
+import Michal86.builder.IronManSuit;
 
 public class Main {
     public static void main(String[] args){
+        //Test Builder
+        IronManSuit model1_0 = new IronManSuit.Builder("Mark I")
+                .color("Steel Silver")
+                .specialFeatures("Jet Boots")
+                .safeFlyingHight(20)
+                .status("Destroyed")
+                .create();
 
+        IronManSuit model1_1 = new IronManSuit.Builder("Mark I")
+                .color("Steel Silver")
+                .specialFeatures("Jet Boots 2")
+                .safeFlyingHight(21)
+                .status("Destroyed")
+                .create();
+
+        System.out.println(model1_0);
+        System.out.println(model1_1);
     }
 
 }
---
+++ 1_Builder/src/Michal86/builder/IronManSuit.java
@@ -0,0 +1,83 @@
+package Michal86.builder;
+
+public class IronManSuit {
+
+    private final String model;
+    private String color;
+    private String specialFeatures;
+    private int safeFlyingHight;
+    private String status;
+
+    private IronManSuit(final Builder builder){
+        this.model = builder.model;
+        this.color = builder.color;
+        this.specialFeatures = builder.specialFeatures;
+        this.safeFlyingHight = builder.safeFlyingHight;
+        this.status = builder.status;
+    }
+
+    //-- getters --
+    public String getModel() {
+        return model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getSpecialFeatures() {
+        return specialFeatures;
+    }
+
+    public int getSafeFlyingHight() {
+        return safeFlyingHight;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    @Override
+    public String toString() {
+        return "[Model: "+model+", color: "+color+", features: "+specialFeatures+
+                ", safe altitude: "+safeFlyingHight+", status: "+ status +"]\n";
+    }
+
+    //=== My Builder ===
+    public static class Builder{
+        private final String model;
+        private String color;
+        private String specialFeatures;
+        private int safeFlyingHight;
+        private String status;
+
+        public Builder(String model){
+            this.model = model;
+        }
+
+        public Builder color(String color){
+            this.color = color;
+            return this;
+        }
+
+        public Builder specialFeatures(String specialFeatures){
+            this.specialFeatures = specialFeatures;
+            return this;
+        }
+
+        public Builder safeFlyingHight(int safeFlyingHight){
+            this.safeFlyingHight = safeFlyingHight;
+            return this;
+        }
+
+        public Builder status(String status){
+            this.status = status;
+            return this;
+        }
+
+        public IronManSuit create(){
+            return new IronManSuit(this);
+        }
+    }
+    //===
+}
